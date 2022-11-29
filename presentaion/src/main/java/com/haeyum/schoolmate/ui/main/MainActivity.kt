2e7a3ca1c9324abea793e7d87d2a6c5c85acbfcc
Holy-Knight <vnycall74@naver.com>
2022-11-29 18:49:25 +0900

    Add bottom navigation component

--- presentaion/src/main/java/com/haeyum/schoolmate/ui/main/MainActivity.kt
+++ presentaion/src/main/java/com/haeyum/schoolmate/ui/main/MainActivity.kt
@@ -3,13 +3,18 @@ package com.haeyum.schoolmate.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.haeyum.schoolmate.R
 import com.haeyum.schoolmate.ui.theme.SchoolmateTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,28 +23,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SchoolmateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SchoolmateTheme {
-        Greeting("Android")
     }
 }
