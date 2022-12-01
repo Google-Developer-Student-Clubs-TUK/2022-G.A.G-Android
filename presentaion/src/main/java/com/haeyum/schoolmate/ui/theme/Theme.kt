acf419005b7044a126d1298a7c2397a0ba174d7a
Holy-Knight <vnycall74@naver.com>
2022-12-01 19:50:23 +0900

    Add pretendard font

--- presentaion/src/main/java/com/haeyum/schoolmate/ui/theme/Theme.kt
+++ presentaion/src/main/java/com/haeyum/schoolmate/ui/theme/Theme.kt
@@ -6,6 +6,10 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.haeyum.schoolmate.R
 
 private val DarkColorPalette = darkColors(
     primary = LightBlue,
@@ -23,6 +27,18 @@ private val LightColorPalette = lightColors(
     background = Color.White,
     surface = Color.White,
     error = Orange
+)
+
+val PretendardFontFamily = FontFamily(
+    Font(resId = R.font.pretendard_thin, weight = FontWeight.Thin),
+    Font(resId = R.font.pretendard_light, weight = FontWeight.Light),
+    Font(resId = R.font.pretendard_extra_light, weight = FontWeight.ExtraLight),
+    Font(resId = R.font.pretendard_black, weight = FontWeight.Black),
+    Font(resId = R.font.pretendard_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.pretendard_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.pretendard_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.pretendard_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.pretendard_extra_bold, weight = FontWeight.ExtraBold),
 )
 
 @Composable
