f2d5a0d5bcb00aebf8d6fd52d3edcd24dce19dbb
Holy-Knight <vnycall74@naver.com>
2022-12-08 01:59:15 +0900

    Feature status bar color changes as the theme color

--- presentaion/src/main/java/com/haeyum/schoolmate/ui/main/MainActivity.kt
+++ presentaion/src/main/java/com/haeyum/schoolmate/ui/main/MainActivity.kt
@@ -3,7 +3,10 @@ package com.haeyum.schoolmate.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.haeyum.schoolmate.ui.theme.SchoolmateTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,7 +16,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SchoolmateTheme {
+                val systemUiController = rememberSystemUiController()
+                val statusBarColor = MaterialTheme.colors.background
+
                 MainScreen(onFinishEvent = ::finish)
+
+                LaunchedEffect(statusBarColor) {
+                    systemUiController.setStatusBarColor(statusBarColor)
+                }
             }
         }
         WindowCompat.setDecorFitsSystemWindows(window, false)
