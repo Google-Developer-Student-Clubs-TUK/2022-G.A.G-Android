e5d28d21e8331379b22a32168334b8bb1d6244a6
Holy-Knight <vnycall74@naver.com>
2022-12-09 00:19:49 +0900

    Feature terms

---
+++ presentaion/src/main/java/com/haeyum/schoolmate/ui/intro/TermsScreen.kt
@@ -0,0 +1,22 @@
+/*
+ * Created by PangMoo on 2022/12/8
+ */
+
+package com.haeyum.schoolmate.ui.intro
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.haeyum.schoolmate.ui.theme.SchoolmateTheme
+
+@Composable
+fun TermsScreen() {
+
+}
+
+@Preview
+@Composable
+fun TermsScreenPreview() {
+    SchoolmateTheme {
+        TermsScreen()
+    }
+}
